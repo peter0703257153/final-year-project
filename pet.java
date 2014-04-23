914f3f1310a6812a4d653903867dc95adb64a6dd
petrelli <peternankunda@gmail.com>
2014-04-23 16:58:09 +0300

    two prints statement added

--- pet.java
+++ pet.java
@@ -2,5 +2,7 @@
 public class pet {
     public static void main(String[] args) {
      System.out.println("peter");
+        System.out.println("peter");
+        System.out.println("peter");
     }
 }
