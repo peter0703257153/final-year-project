4a674ae8904831cf17c212d9b33d43362c29e5f7
petrelli <peternankunda@gmail.com>
2014-04-23 17:02:00 +0300

    public class pet {     public static void main(String[] args) {      System.out.println("peter");         System.out.println("peter");         System.out.println("peternan");     } }

--- pet.java
+++ pet.java
@@ -3,6 +3,6 @@ public class pet {
     public static void main(String[] args) {
      System.out.println("peter");
         System.out.println("peter");
-        System.out.println("peter");
+        System.out.println("peternan");
     }
 }
